bf966132c28f159ec47933767cc1ca81da1842e0
John Rice <john.rice@revature.com>
2020-11-10 20:36:39 -0500

    updated app to read JSON converted tweet data

--- nasdaq-data/src/main/scala/nasdaqprices/Runner.scala
+++ nasdaq-data/src/main/scala/nasdaqprices/Runner.scala
@@ -10,16 +10,29 @@ object Runner {
         val conf = new SparkConf().setAppName(appName).setMaster("local[4]")
         val sc = new SparkContext(conf)
         val inputNasdaqData = "TeslaNasdaqPrices.csv"
+        val inputJSONData = "out.csv"
 
         formatNasdaq(sc, inputNasdaqData)
-        
+
         def formatNasdaq(context: SparkContext, file: String) = {
             //format .csv with desirable order and remove "volume" column
             val splitRecords = sc.textFile(file)
                 .map(_.split(","))
-            val orderFields = splitRecords.map(arr => (arr(0), arr(3), arr(4), arr(5), arr(1)))
+            val orderFields = splitRecords
+                .map(arr => (arr(0), arr(3), arr(4), arr(5), arr(1)))
 
+            //print 10 formatted and ordered Nasdaq prices
             orderFields.take(10).foreach(println)
+
+
+            // Print 10 formatted Tweets data
+            val splitJSON = sc.textFile(inputJSONData)
+                .map(_.split(",").mkString)
+            //testing
+            //val orderJSONFields = splitJSON
+            //    .map(arr => (arr(0), arr(3), arr(4), arr(5), arr(1)))
+
+            splitJSON.take(10).foreach(println)
         }
     }
 
