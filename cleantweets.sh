35eb81e64cd5ad1d0ea5b9b60629c3f8dd6a1756
Kevin Conlin <kevinconlin80@gmail.com>
2020-11-12 09:51:13 -0700

    On branch Kevin  Your branch is up to date with 'origin/Kevin'.
     Changes to be committed:
    	new file:   cleantweets.sh

---
+++ cleantweets.sh
@@ -0,0 +1,15 @@
+#!
+
+INPUT=$1
+OUTPUT=$2
+
+
+for i in `seq 0 7`;
+do
+    DATE=`jshon -e data < $INPUT | jshon -e $i -e created_at | cut -c2-11`
+    ID=`jshon -e data < $INPUT | jshon -e $i -e id`
+    TEXT=`jshon -e data < $INPUT | jshon -e $i -e text`
+    RETWEETS=`jshon -e data < $INPUT | jshon -e $i jshon -e public_metrics -e retweet_count`
+    FAVORITES=`jshon -e data < $INPUT | jshon -e $i jshon -e public_metrics -e like_count`
+    echo "$DATE,$ID,$TEXT,$RETWEETS,$FAVORITES" >> $OUTPUT
+done
